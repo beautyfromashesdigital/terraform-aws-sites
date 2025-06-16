ad12bef7def4e218e3ef0b4eeea5f98288bfabe8
beautyfromashes <tyler@beautyfromashesdigitial.com>
2025-06-16 10:09:21 -0400

    v1.0.6

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,12 @@ output "zone_id" {
 output "name_servers" {
   value = aws_route53_zone.zone.name_servers
 }
+output "validation_record_name" {
+  description = "Name of the TXT record for ACM DNS validation"
+  value       = aws_route53_record.cert_validation.name
+}
+
+output "validation_record_value" {
+  description = "Value of the TXT record for ACM DNS validation"
+  value       = aws_route53_record.cert_validation.records[0]
+}
